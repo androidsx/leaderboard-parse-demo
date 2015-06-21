bb81d7c4334e497bf0b03b7d8b1d52722698c4a0
Pablo Pera <pablo.sx@gmail.com>
2015-06-21 12:53:02 -0400

    Removed superfluous code

--- app/src/main/java/com/androidsx/leaderboarddemo/PickLevelActivity.java
+++ app/src/main/java/com/androidsx/leaderboarddemo/PickLevelActivity.java
@@ -1,22 +1,18 @@
 package com.androidsx.leaderboarddemo;
 
-import android.app.ListActivity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 
 import java.util.Arrays;
 import java.util.List;
 
 
-public class PickLevelActivity extends ActionBarActivity {
+public class PickLevelActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,27 +33,5 @@ public class PickLevelActivity extends ActionBarActivity {
                 finish();
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_pick_level, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
